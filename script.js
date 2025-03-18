8fbced9d70f3e91720e9ccb38085ff49f360e109
Rodrilundain <136036888+Rodrilundain@users.noreply.github.com>
2025-03-18 12:32:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
         inicioBtn.classList.remove("activo");
         formularioCaja.style.transform = "rotateY(180deg)";
     });
+
+    document.getElementById("formularioInicio").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    // Obtener valores de los inputs
+    let usuario = document.querySelector("#formularioInicio input[type='email']").value;
+    let contraseña = document.querySelector("#formularioInicio input[type='password']").value;
+
+    // Validar credenciales
+    if (usuario === "CallCenter@grupofiancar.com" && contraseña === "Fiancar2025") {
+        window.location.href = "https://rodrilundain.github.io/index.callcenter/";
+    } else {
+        alert("Usuario o contraseña incorrectos. Inténtalo nuevamente.");
+    }
+});
+
 });
